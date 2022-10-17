54fe58d5b69d7966b6b361d1feb6791338bc9d9c
Salome Charkviani <salo.charkviani@gmail.com>
2022-10-17 20:56:23 +0400

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+
+function MeetupDetails(){
+    return(
+        <Fragment>
+            <img src='' alt=''/>
+        </Fragment>
+    );
+}
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+
+import { useEffect, useState } from 'react';
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS=[
+    {
+        id:"m1",
+        title: "A First Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "Munich",
+        description: "This is a first meetup!"
+    },
+    {
+        id:"m2",
+        title: "A Second Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Brandenburger_Tor_abends.jpg",
+        address: "Berlin",
+        description: "This is a second meetup!"
+    },
+]
+
+function HomePage(){
+
+return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export async function getStaticProps(){
+
+    return{
+        props:{
+            meetups: DUMMY_MEETUPS
+        }
+    }
+}
+
+export default HomePage;
